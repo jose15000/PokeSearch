9f07ccf6dcf61063aaf1d9b4cc536ffeba3be880
José Henrique <jose1500henrique@hotmail.com>
2023-12-28 00:53:45 -0300

    commit

--- script.js
+++ script.js
@@ -7,7 +7,13 @@ card.style.display = 'none';
 const query = document.getElementById("input");
 const btn = document.querySelector('.btn');
 
+const loader = document.querySelector('.spinner-border');
+loader.style.display = 'none';
+
+
 //função para fazer a requisição
+
+//#region search funcion
 function search(){
     //recuperando o valor do input e passando para a url da api (nome do pokémon/id da pokédex)
     let pokemon = query.value;
@@ -25,14 +31,15 @@ function search(){
     //recuperando os dados da API
     .then(data =>{
         console.log(data)
-
         var pokeName = data['name'];
         var UpperCase = pokeName.charAt(0).toUpperCase() + pokeName.slice(1);
-        document.querySelector('.name').innerHTML ='#' + data['id'] + '\n' + UpperCase;
+
+        var slugifyUpper = slugify(pokeName);
+        document.querySelector('.name').innerHTML ='#' + data['id'] + ' ' + UpperCase;
         document.querySelector('.img-fluid').src = data.sprites.versions["generation-v"]["black-white"].animated.front_default;
         document.querySelector('#xp').innerHTML = 'Experiência base: ' + data.base_experience;
 
-        //iterar pelas habilidades do pokemon, criar novas linhas na lista e retornar os dados no front
+        //#region iterar pelas habilidades do pokemon, criar novas linhas na lista e retornar os dados no front
         const movimentos = document.querySelector('#moves');
         movimentos.innerHTML = '';
         data.moves.forEach(move => {
@@ -41,23 +48,17 @@ function search(){
                             movimentos.appendChild(listItem);
                             
         });
-    
+        //#endregion
+
         document.querySelector('#peso').innerHTML = 'Peso: ' + data.weight/10 + ' Kg';  
-        document.querySelector('#altura').innerHTML = 'Altura: ' + data.height/10 + ' m'
+        document.querySelector('#altura').innerHTML = 'Altura: ' + data.height/10 + ' m';
+
         card.style.display = 'block';
     })
 }
 
 //chama a função e faz a requisição para a api
 btn.onclick = search;
-
-
-
-
-
-
-
-
 
 
 
